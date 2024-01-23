9ab02da87db48952a22f756a407c5afe15c38571
Simen Smehaugen <simdevgen@outlook.com>
2024-01-23 09:51:18 +0100

    added DELETE and POST requests between frontend and backend

--- src/main/java/com/otrumchallenge/otrumcase/NameListApi.java
+++ src/main/java/com/otrumchallenge/otrumcase/NameListApi.java
@@ -3,20 +3,17 @@ package com.otrumchallenge.otrumcase;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @RestController
 @RequestMapping("/name-list")
 @CrossOrigin(origins = "http://localhost:5173")
 public class NameListApi {
+
     private final List<Person> people;
 
     public NameListApi() {
@@ -54,6 +51,21 @@ public class NameListApi {
     public ResponseEntity<Void> addPerson(@RequestBody Person newPerson) {
         people.add(newPerson);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping(value = "/{personId}")
+    public ResponseEntity<Void> deletePerson(@PathVariable String personId) {
+        try {
+            int index = Integer.parseInt(personId);
+            if (index >= 0 && index < people.size()) {
+                people.remove(index);
+                return ResponseEntity.noContent().build();
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     private static class Person {
